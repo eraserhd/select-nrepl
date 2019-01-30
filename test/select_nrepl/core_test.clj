22e483bdcfa7c5682016e91329fd18201a687b1c
Jason Felice <jason.m.felice@gmail.com>
2019-01-30 14:27:26 -0500

    Get the test harness together

--- test/select_nrepl/core_test.clj
+++ test/select_nrepl/core_test.clj
@@ -3,8 +3,43 @@
    [clojure.test :refer :all]
    [select-nrepl.core]))
 
-(defn- select [kind text]
-  text)
+(defn- parse-input
+  [text]
+  (reduce
+    (fn [state ch]
+      (case ch
+        \<       (assoc state :start (:position state))
+        \>       (assoc state :end (:position state))
+        \newline (update state :position (fn [[i j]] [(inc i) 0]))
+        (-> state
+          (update-in [:position 1] inc)
+          (update :text str ch))))
+    {:text ""
+     :position [0 0]
+     :start nil
+     :end nil}
+    (seq text)))
+
+(defn- compose-output
+  [text start end]
+  (let [result
+        (:text (reduce
+                (fn [state ch]
+                  (cond-> state
+                    (= start (:position state)) (update :text str \<)
+                    (= end (:position state))   (update :text str \>)
+                    true                        (update :text str ch)
+                    (= ch \newline)             (update :position (fn [[i j]] [(inc i) 0]))
+                    (not= ch \newline)          (update-in [:position 1] inc)))
+                {:text ""
+                 :position [0 0]}
+                (concat (seq text) [\space])))]
+    (subs result 0 (dec (count result)))))
+
+(defn- select [kind input]
+  (let [{:keys [text start end]} (parse-input input)
+        [start end] (select-nrepl.core/select kind text start end)]
+    (compose-output text start end)))
 
 (deftest a-select-element
   (are [before after] (= (select :element before) after)
