cf209fac064806ad408a0a5ec8b88e9d81228a2f
Jason Felice <jason.m.felice@gmail.com>
2019-02-08 14:07:35 -0500

    Don't bring in clojure dependency

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "http://github.com/eraserhd/select-nrepl"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.reader "1.2.2"]]
+  :dependencies [[org.clojure/tools.reader "1.2.2"]]
+  :excludes [org.clojure/clojure]
   :repl-options {:init-ns dev}
   :plugins [[lein-midje "3.2.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [org.clojure/tools.namespace "0.2.11"]
                                   [midje "1.9.4"]]
                    :repl-options {:nrepl-middleware [select-nrepl.core/wrap-select]}}})
