99ad5f1cbb9ee1bf2a0519337739fb52ad2b580b
Jason Felice <jason.m.felice@gmail.com>
2019-03-20 11:05:32 -0400

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.eraserhead/select-nrepl "0.1.0"
+(defproject net.eraserhead/select-nrepl "0.1.1-SNAPSHOT"
   :description "Text object support as nREPL middleware"
   :url "http://github.com/eraserhd/select-nrepl"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
